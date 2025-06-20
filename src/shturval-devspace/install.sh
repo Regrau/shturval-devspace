310fb774c868a1024fbd876253df5899b5b9efc7
Georg Grauberger <g.grauberger@jet.su>
2025-06-20 14:50:07 +0300

    Fix su command

--- src/shturval-devspace/install.sh
+++ src/shturval-devspace/install.sh
@@ -23,9 +23,9 @@ curl -L https://public.shturval.tech/stc-$STC_VERSION -o /usr/local/bin/stc
 chmod +x /usr/local/bin/stc
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-su $_REMOTE_USER -c helm repo add shturval https://r.shturval.tech/repository/shturval_helm
+su $_REMOTE_USER -c "helm repo add shturval https://r.shturval.tech/repository/shturval_helm"
 
 curl -L https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
 
-su $_REMOTE_USER -c  cat 'source <(command kubectl completion zsh)' >> /home/$_REMOTE_USER/.zshrc
+su $_REMOTE_USER -c  "cat 'source <(command kubectl completion zsh)' >> /home/$_REMOTE_USER/.zshrc"
